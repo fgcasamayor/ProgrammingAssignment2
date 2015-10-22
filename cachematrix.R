83c2e128e1e6fef707f433c8ee662f52481baa59
fgcasamayor <pako_casamay90@hotmail.com>
2015-10-22 17:08:57 +0200

    Functions without comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,35 @@
 ##Git TEST
 
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
 
+  get <- function() {
+    x
+  }
+  
+  setinverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  getinverse <- function(){
+    i
+  }
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
@@ -14,4 +37,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinverse()
+  
+  if(!is.null(i)) {
+    message("Getting cached data.")
+    return(i)
+  }
+  
+  data <- x$get()
+  
+  i <- solve(data)
+  
+  x$setinverse(i)
+  
+  i
+  
 }
+
+
+
